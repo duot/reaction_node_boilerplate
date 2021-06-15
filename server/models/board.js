91e1cef512653b4733340c2f4e0e210fb2267845
James Duot <duot.jim@gmail.com>
2021-06-15 09:14:32 -0700

    feat: board model references lists model

--- server/models/board.js
+++ server/models/board.js
@@ -1,13 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const BoardSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, 'The Board title is required']
+const { ObjectId } = Schema.Types;
+const BoardSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "The Board title is required"],
+    },
+    lists: [
+      {
+        type: ObjectId,
+        ref: "List",
+      },
+    ],
+  },
+  {
+    timestamps: true,
   }
-})
+);
 
-const Board = mongoose.model('Board', BoardSchema);
+const Board = mongoose.model("Board", BoardSchema);
 
 module.exports = Board;
