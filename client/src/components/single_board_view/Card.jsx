35eb6a91214d8302553a3f703965b8add7ea5b16
James Duot <duot.jim@gmail.com>
2021-06-17 09:41:18 -0700

    refactor: card shows card deatails including date and description

--- client/src/components/single_board_view/Card.jsx
+++ client/src/components/single_board_view/Card.jsx
@@ -4,28 +4,43 @@ import CardLabel from "./CardLabel"
 
 export default function Card({ cardId }) {
 	const card = useSelector(state => state.cards.find((card) => {
-    return card._id === cardId
-  }))
+		return card._id === cardId
+	}))
+
+	const date = new Date(card.dueDate)
+	const months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec",
+	]
+
+	const dateString = months[date.getMonth()] + " " + date.getDate()
 
 	return (
 		<div className="card-background">
 			<div className="card ">
-					<i className="edit-toggle edit-icon sm-icon"></i>
-					<div className="card-info">
-						{card.labels.map((label) => <CardLabel key={label} label={label} />)}
-						<p>
-							Cards do many cool things. Click on this card to
-							open it and learn more...
-						</p>
-					</div>
-					<div className="card-icons">
-						<i className="clock-icon sm-icon overdue-recent completed">
-							Aug 4
-						</i>
-						<i className="description-icon sm-icon"></i>
-						<i className="comment-icon sm-icon"></i>
-					</div>
+				<i className="edit-toggle edit-icon sm-icon"></i>
+				<div className="card-info">
+					{card.labels.map((label) => <CardLabel key={label} label={label} />)}
+					<p>{card.description}</p>
+				</div>
+				<div className="card-icons">
+					<i className="clock-icon sm-icon overdue-recent completed">
+						{dateString}
+					</i>
+					<i className="description-icon sm-icon"></i>
+					<i className="comment-icon sm-icon"></i>
 				</div>
 			</div>
+		</div>
 	)
 }
