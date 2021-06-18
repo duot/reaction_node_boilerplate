5155edab315dced998668d45e6a99838d532abe3
“Jordan <jordan.thomas789@gmail.com>
2021-06-18 13:39:08 -0400

    feat: list title becomes a text box on click

--- client/src/components/single_board_view/List.jsx
+++ client/src/components/single_board_view/List.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Card from "./Card"
 
 export default function List({ listId }) {
-  /// populate state of list
-  const dispatch = useDispatch()
+  const [titleEditable, setTitleEditable] = useState(false);
+  const [editedTitle, setEditedTitle] = useState("")
+  //^change so default value is current list title
   const cards = useSelector(state => state.cards.filter((card) => {
     return card.listId === listId
   }))
@@ -13,41 +14,53 @@ export default function List({ listId }) {
     return list._id === listId
   }))
 
-return (
-  <div className="list-wrapper">
-    <div className="list-background">
-      <div className="list">
-        <a className="more-icon sm-icon" href=""></a>
-        <div>
-          <p className="list-title">{list.title}</p>
-        </div>
-        <div className="add-dropdown add-top">
-          <div className="card"></div>
-          <a className="button">Add</a>
-          <i className="x-icon icon"></i>
-          <div className="add-options">
-            <span>...</span>
+  const titleClicked = () => {
+    setTitleEditable(true)
+  }
+
+  const titleUnclicked = () => {
+    setTitleEditable(false)
+  }
+
+  return (
+    <div className="list-wrapper">
+      <div className="list-background">
+        <div className="list">
+          <a className="more-icon sm-icon" href=""></a>
+          <div>
+            {titleEditable ?
+              <input autoFocus="autofocus" type="text" value={editedTitle}
+                onBlur={titleUnclicked} onChange={(e) => setEditedTitle(e.target.value)}></input> :
+              <p className="list-title" onClick={titleClicked}>{list.title}</p>}
           </div>
-        </div>
-        <div id="cards-container" data-id="list-1-cards">
-          {cards.map(card => <Card key={card._id} cardId={card._id} />)}
-        </div>
-        <div className="add-dropdown add-bottom">
-          <div className="card">
-            <div className="card-info"></div>
-            <textarea name="add-card"></textarea>
-            <div className="members"></div>
+          <div className="add-dropdown add-top">
+            <div className="card"></div>
+            <a className="button">Add</a>
+            <i className="x-icon icon"></i>
+            <div className="add-options">
+              <span>...</span>
+            </div>
           </div>
-          <a className="button">Add</a>
-          <i className="x-icon icon"></i>
-          <div className="add-options">
-            <span>...</span>
+          <div id="cards-container" data-id="list-1-cards">
+            {cards.map(card => <Card key={card._id} cardId={card._id} />)}
           </div>
-        </div>
-        <div className="add-card-toggle" data-position="bottom">
-          Add a card...
+          <div className="add-dropdown add-bottom">
+            <div className="card">
+              <div className="card-info"></div>
+              <textarea name="add-card"></textarea>
+              <div className="members"></div>
+            </div>
+            <a className="button">Add</a>
+            <i className="x-icon icon"></i>
+            <div className="add-options">
+              <span>...</span>
+            </div>
+          </div>
+          <div className="add-card-toggle" data-position="bottom">
+            Add a card...
+          </div>
         </div>
       </div>
     </div>
-  </div>
-)}
+  )
+}
